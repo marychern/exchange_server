c5f2aabfb5536550e55faa8de003330aba42bd82
Daniel Thurau <Daniel.n.thurau@gmail.com>
2018-03-12 13:09:47 -0700

    Real time printing of message received for demo

--- OuchServer/ouch_server.py
+++ OuchServer/ouch_server.py
@@ -88,6 +88,7 @@ class ProtocolMessageServer(object):
                 break
 
             client_msg = message_type.from_bytes(payload_bytes, header=False)
+            print("Message recieved from client: %s" % str(client_msg))
             client_msg.meta = client_token
             await self.broadcast_to_listeners(client_msg)
             
